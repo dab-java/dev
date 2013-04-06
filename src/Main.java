080585bf6e6e0e77b6b8ac8372d69c71ce463f8d
clynfirst <clynfirst@free.fr>
2013-04-06 19:53:07 +0200

    Base

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Projet Java Distributeur Automatique de Billets (DAB) par :
+ * - Maurice Phutthavong
+ * - Charles Bizet
+ * - Guillaume Cornet
+ */
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bienvenue sur le Distributeur Automatique de Billets 2013");
+	}
+}
